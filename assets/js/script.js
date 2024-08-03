e9df90c952c5fe3bc10af008f21919989d0dba80
Hebz0320 <hebz0320@gmail.com>
2024-08-03 13:34:17 -0500

    Update script.js
    Fixing the error : TypeError: Cannot set properties of null (setting 'textContent')
        at script.js:12:33 & TypeError: Cannot set properties of null (setting 'textContent')
        at script.js:12:33

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,45 @@
 // Counter
-
 document.addEventListener("DOMContentLoaded", () => {
     function counter(id, start, end, duration) {
-        let obj = document.getElementById(id),
-            current = start,
-            range = end - start,
-            increment = end > start ? 1 : -1,
-            step = Math.abs(Math.floor(duration / range)),
-            timer = setInterval(() => {
-                current += increment;
-                obj.textContent = current;
-                if (current == end) {
-                    clearInterval(timer);
-                }
-            }, step);
+        let obj = document.getElementById(id);
+        if (obj) { // Check if the element exists
+            let current = start,
+                range = end - start,
+                increment = end > start ? 1 : -1,
+                step = Math.abs(Math.floor(duration / range)),
+                timer = setInterval(() => {
+                    current += increment;
+                    obj.textContent = current;
+                    if (current == end) {
+                        clearInterval(timer);
+                    }
+                }, step);
+        }
     }
+
+    // Initialize counters only if elements are present
     counter("count1", 0, 1287, 3000);
     counter("count2", 100, 5786, 2500);
     counter("count3", 0, 1440, 3000);
     counter("count4", 0, 7110, 3000);
-
 });
 
-
 // Scroll 
-
 window.addEventListener('scroll', reveal);
 
-function reveal(){
+function reveal() {
     var reveals = document.querySelectorAll('.reveal');
 
-    for(var i =0; i< reveals.length; i++){
-
+    for (var i = 0; i < reveals.length; i++) {
         var windowheight = window.innerHeight;
         var revealtop = reveals[i].getBoundingClientRect().top;
         var revealpoint = 150;
 
-        if(revealtop < windowheight - revealpoint){
+        if (revealtop < windowheight - revealpoint) {
             reveals[i].classList.add('active');
-        }else{
+        } else {
             reveals[i].classList.remove('active');
         }
     }
 }
+
