025f83dafa8cba8a27268cdcc56952187a261c81
Hebz0320 <hebz0320@gmail.com>
2024-08-04 15:31:52 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
             }, step);
     }
     counter("count1", 0, 1287, 3000);
-    counter("count2", 100, 5786, 2500);
+    counter("count2", 100, 3219, 2500);
     counter("count3", 0, 1440, 3000);
     counter("count4", 0, 7110, 3000);
 
